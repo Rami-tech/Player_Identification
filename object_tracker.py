9fbfbac1b6bebf07e42c02eb67cf8726d6ef449a
Rami Naffah <53605327+Rami-tech@users.noreply.github.com>
2021-10-17 19:49:48 +0300

    Update object_tracker.py

--- object_tracker.py
+++ object_tracker.py
@@ -41,7 +41,7 @@ flags.DEFINE_boolean('count', False, 'count objects being tracked on screen')
 
 def main(_argv):
     path = "data/info"
-    if(!os.path.exists(path)):
+    if(not os.path.exists(path)):
         os.mkdir(path)
     # Definition of the parameters
     max_cosine_distance = 0.4
